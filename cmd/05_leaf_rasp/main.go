b5e2995397fb3519654aea0725ca24bcd52dc4f3
Loh Siu Yin <siuyin@beyondbroadcast.com>
2026-01-17 09:08:58 +0800

    feat: add mirror kv to rasp

---
+++ cmd/05_leaf_rasp/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	"github.com/nats-io/nats-server/v2/server"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	"github.com/siuyin/dflt"
+)
+
+var ctx = context.Background()
+
+type leaf1 struct {
+	nc *nats.Conn
+	ns *server.Server
+	js jetstream.JetStream
+	kv jetstream.KeyValue
+}
+
+func newLeaf1(ctx context.Context, name, source, srcDomain string) (*leaf1, error) {
+	var err error
+	l := leaf1{}
+	l.nc, l.ns, err = embedNATSServer()
+	if err != nil {
+		return nil, fmt.Errorf("newLeaf1: %v", err)
+	}
+
+	l.js, err = jetstream.New(l.nc)
+	if err != nil {
+		return nil, fmt.Errorf("newLeaf1: jetstream ", err)
+	}
+
+	l.kv, err = l.js.CreateOrUpdateKeyValue(ctx, jetstream.KeyValueConfig{Bucket: name + "kv", Mirror: &jetstream.StreamSource{Name: "KV_" + source + "kv", Domain: srcDomain}})
+	if err != nil {
+		return nil, fmt.Errorf("newLeaf1 create kv: %v", err)
+	}
+
+	return &l, nil
+}
+
+func main() {
+	lf, err := newLeaf1(ctx, "my", "my", "rasp")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer lf.nc.Close()
+	defer lf.ns.WaitForShutdown() // requires a ctrl-C to terminate
+	//defer lf.ns.Shutdown()
+}
+
+func embedNATSServer() (*nats.Conn, *server.Server, error) {
+	dir := dflt.EnvString("STORE_DIR", "/home/siuyin/embedded_nats")
+	passwd := dflt.EnvString("LEAF_PASSWD", "your leaf connection password here")
+	host := dflt.EnvString("LEAF_HOST", "rasp.beyondbroadcast.com:8080")
+	log.Printf("STORE_DIR=%s LEAF_PASSWD=%s LEAF_HOST=%s", dir, passwd[0:5]+"...", host)
+	opts := &server.Options{ServerName: "leaf", JetStream: true, StoreDir: dir, JetStreamDomain: "leaf", Port: 4222, DontListen: false,
+		LeafNode: server.LeafNodeOpts{Remotes: []*server.RemoteLeafOpts{
+			&server.RemoteLeafOpts{URLs: []*url.URL{
+				&url.URL{Scheme: "tls", Host: host, User: url.UserPassword("a", passwd)}},
+			}},
+		},
+	}
+
+	ns, err := server.NewServer(opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ns.ConfigureLogger()
+	ns.Start()
+	if !ns.ReadyForConnections(5 * time.Second) {
+		return nil, nil, errors.New("could not start embedded NATS server within 5 seconds")
+	}
+
+	nc, err := nats.Connect(ns.ClientURL(), nats.InProcessServer(ns))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return nc, ns, nil
+}
