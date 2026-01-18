2adc595c5066d75262c16c7a1fbda221ef44d4ad
Loh Siu Yin <siuyin@beyondbroadcast.com>
2026-01-18 09:55:19 +0800

    feat: create remote stream

--- cmd/05_leaf_rasp/main.go
+++ cmd/05_leaf_rasp/main.go
@@ -19,7 +19,8 @@ var ctx = context.Background()
 type leaf struct {
 	nc *nats.Conn
 	ns *server.Server
-	js jetstream.JetStream
+	js jetstream.JetStream // local JetStream context
+	jr jetstream.JetStream // remote JetStream context
 	kv jetstream.KeyValue
 }
 
@@ -28,17 +29,23 @@ func newLeaf(ctx context.Context, name, source, srcDomain string) (*leaf, error)
 	l := leaf{}
 	l.nc, l.ns, err = embedNATSServer()
 	if err != nil {
-		return nil, fmt.Errorf("newLeaf1: %v", err)
+		return nil, fmt.Errorf("newLeaf: %v", err)
 	}
 
 	l.js, err = jetstream.New(l.nc)
 	if err != nil {
-		return nil, fmt.Errorf("newLeaf1: jetstream ", err)
+		return nil, fmt.Errorf("newLeaf: jetstream: %v", err)
+	}
+
+	remDomain := dflt.EnvString("REMOTE_JETSTREAM_DOMAIN", "rasp")
+	l.jr, err = jetstream.NewWithDomain(l.nc, remDomain)
+	if err != nil {
+		return nil, fmt.Errorf("newLeaf: jetstream with domain: %v", err)
 	}
 
 	l.kv, err = l.js.CreateOrUpdateKeyValue(ctx, jetstream.KeyValueConfig{Bucket: name + "kv", Mirror: &jetstream.StreamSource{Name: "KV_" + source + "kv", Domain: srcDomain}})
 	if err != nil {
-		return nil, fmt.Errorf("newLeaf1 create kv: %v", err)
+		return nil, fmt.Errorf("newLeaf create kv: %v", err)
 	}
 
 	return &l, nil
@@ -48,6 +55,24 @@ func (l *leaf) hiV1() {
 	l.nc.Subscribe("v1.hi", func(m *nats.Msg) {
 		m.Respond([]byte(fmt.Sprintf("Greetings %s: Nice to meet you. The time is %s.\n", string(m.Data), time.Now().Format("15:04:05.000 -0700"))))
 	})
+}
+
+func (l *leaf) rstrm(ctx context.Context, name string, subs []string) (jetstream.Stream, error) {
+	for {
+		_, err := l.jr.AccountInfo(ctx)
+		if err != nil {
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		break
+	}
+
+	rstrm, err := l.jr.CreateOrUpdateStream(ctx, jetstream.StreamConfig{
+		Name: name, Subjects: subs, MaxAge: 10 * time.Minute})
+	if err != nil {
+		return nil, fmt.Errorf("leaf rstrm: create stream: %s: %v", name, err)
+	}
+	return rstrm, nil
 }
 
 func main() {
@@ -61,6 +86,16 @@ func main() {
 	//defer lf.ns.Shutdown()
 
 	lf.hiV1()
+
+	ctx := context.Background()
+	mstrm, err := lf.rstrm(ctx, "mstrm", []string{"m.>"})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	_ = mstrm
+	log.Println("remote stream mstrm created")
 }
 
 func embedNATSServer() (*nats.Conn, *server.Server, error) {
